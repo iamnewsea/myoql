1665a2ed7b01e9c91673cdf55be2962c25108e13
于新海 <yuxinhai@lwljuyang.com>
2019-11-24 13:48:34 +0800

    .

--- kotlin/ktext/kotlin/nbcp/base/extend/MyObject_List.kt
+++ kotlin/ktext/kotlin/nbcp/base/extend/MyObject_List.kt
@@ -92,13 +92,12 @@ inline fun <T> Iterator<T>.Filter(predicate: (T) -> Boolean): MutableList<T> {
 
     return list;
 }
-
 /*
 比较两个数组的内容是否相同, 去除相同数据进行比较 .如:
 [1,1,2] .equalArrayContent( [1,2,2] )  == true
  */
-infix fun Array<*>.EqualArrayContent(other: Array<*>): Boolean {
-    return this.toList().EqualArrayContent(other.toList());
+fun Array<*>.EqualArrayContent(other: Array<*>, withIndex:Boolean = false): Boolean {
+    return this.toList().EqualArrayContent(other.toList(),withIndex);
 }
 
 
@@ -107,10 +106,20 @@ infix fun Array<*>.EqualArrayContent(other: Array<*>): Boolean {
 比较两个数组的内容是否相同, 去除相同数据进行比较 .如:
 [1,1,2] .equalArrayContent( [1,2,2] )  == true
  */
-infix fun Collection<*>.EqualArrayContent(other: Collection<*>): Boolean {
+fun Collection<*>.EqualArrayContent(other: Collection<*>,withIndex:Boolean = false): Boolean {
     if (this.size == 0 && other.size == 0) return true;
     else if (this.size == 0) return false;
     else if (other.size == 0) return false;
+
+    if( withIndex){
+        this.forEachIndexed{ index,item->
+            var otherItem = other.elementAt(index);
+            if( item != otherItem){
+                return false;
+            }
+        }
+        return true;
+    }
 
     var one = this.distinct();
     var two = other.distinct();
@@ -119,7 +128,6 @@ infix fun Collection<*>.EqualArrayContent(other: Collection<*>): Boolean {
     if (one.size != two.size) return false;
     return one.intersect(two).size == this.size;
 }
-
 
 inline fun <T> List<out T>.Skip(skipNumber: Int): List<T> {
     var ret = mutableListOf<T>();
