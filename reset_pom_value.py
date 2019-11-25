a0a25b8fbbd86217c989e75315106208ae9aefd4
于新海 <yuxinhai@lwljuyang.com>
2019-11-25 21:03:57 +0800

    .

--- reset_pom_value.py
+++ reset_pom_value.py
@@ -4,34 +4,41 @@
 import os
 import sys
 import shutil
-import xpath
-import  xml.dom.minidom
-import  getopt
+import getopt
+from lxml import etree
 
 
 def err(message):
-    print("%s 发生了错误！！！" %(message))
+    print("%s 发生了错误！！！" % (message))
     sys.exit(1)
 
+
 def replaceOsSep(src):
-    return src.replace("/",os.sep).replace("\\",os.sep)
+    return src.replace("/", os.sep).replace("\\", os.sep)
+
 
 def setWorkPath():
-    base_path =  os.path.abspath( os.path.join( __file__ ,"../" ) )
-    os.chdir( base_path)
+    base_path = os.path.abspath(os.path.join(__file__, "../"))
+    os.chdir(base_path)
 
-def getArgs( ):
+def printHelp():
+    print('''
+替换 xml 文件中的值。
+python reset_pom_value.py -f pom.xml所在路径（默认当前） -t xpath表达式 -v 新的值
+''')
+
+def getArgs():
     tag = ""
     value = ""
     file = ""
     try:
-        opts, args = getopt.getopt(sys.argv[1:],"t:v:fh",["tag=","value=","file="])
+        opts, args = getopt.getopt(sys.argv[1:], "t:v:fh", ["tag=", "value=", "file="])
     except getopt.GetoptError:
-        print ('reset_pom_value.py -f pom.xml所在路径（默认当前） -t xpath表达式 -v 新的值')
+        printHelp()
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print ('reset_pom_value.py -f pom.xml所在路径（默认当前） -t xpath表达式 -v 新的值')
+            printHelp()
             sys.exit()
         elif opt in ("-f", "--file"):
             file = arg
@@ -40,17 +47,16 @@ def getArgs( ):
         elif opt in ("-v", "--value"):
             value = arg
 
+    return tag, value, file
 
-    return file
-def check_same(module,groupId,artifactId):
+
+def check_same(module, groupId, artifactId):
     os.chdir(module)
-    dom = xml.dom.minidom.parse('pom.xml')
-    root = dom.documentElement
+    dom = etree.parse("pom.xml")
+    nsmap = dom.getroot().nsmap[None]
 
-    vv_data = root.getElementsByTagName('version')[0].childNodes[0];
-    mns = list(filter(lambda x: x.nodeType == 1, root.getElementsByTagName('parent')[0].childNodes));
-    t_groupId= list(filter( lambda x: x.tagName == "groupId",mns ))[0].childNodes[0].data
-    t_artifactId= list(filter( lambda x: x.tagName == "artifactId",mns))[0].childNodes[0].data
+    t_groupId = dom.find("{%s}parent/{%s}groupId" % (nsmap, nsmap)).text
+    t_artifactId = dom.find("{%s}parent/{%s}artifactId" % (nsmap, nsmap)).text
 
     if t_groupId != groupId:
         return False
@@ -60,56 +66,62 @@ def check_same(module,groupId,artifactId):
 
     return True
 
-def getSubPoms():
-    dom = xml.dom.minidom.parse('pom.xml')
-    root = dom.documentElement
-    groupId = root.getElementsByTagName('groupId')[0].childNodes[0].data;
-    artifactId = root.getElementsByTagName('artifactId')[0].childNodes[0].data;
 
-    mns = filter(lambda x: x.nodeType == 1, root.getElementsByTagName('modules')[0].childNodes);
-    modules = list( map(lambda x: x.childNodes[0].data ,mns) )
+def getSubPoms():
+    dom = etree.parse("pom.xml")
+    nsmap = dom.getroot().nsmap[None]
+
+    groupId = dom.find("{%s}groupId" % (nsmap)).text
+    artifactId = dom.find("{%s}artifactId" % (nsmap)).text
+    modules = list(map(lambda x: x.text, dom.findall("{%s}modules/{%s}module" % (nsmap, nsmap))))
+
+    # etree.tostring(dom,doctype="xml")
 
     # 检查子项目的 parent 是否一致
     for index in range(len(modules)):
-        m = modues[index]
-        if not check_same(m,groupId,artifactId):
+        m = modules[index]
+        if not check_same(m, groupId, artifactId):
             modules.remove(m)
         os.chdir("..")
 
     return modules
 
-def resetData(module,tag,value):
+
+def resetData(module, tag, value):
     os.chdir(module);
-    dom = xml.dom.minidom.parse('pom.xml')
-    root = dom.documentElement
-    elems = xpath.find(tag,root)
-    for e in elems:
-        e.data = value
+
+    dom = etree.parse("pom.xml")
+    nsmap = dom.getroot().nsmap[None]
+
+    path = "/".join(list(map(lambda x: "{%s}%s" % (nsmap, x), tag.split("/"))))
+    # print (path);
+    dom.find(path).text = value
+
+    # xmlContent = etree.tostring(dom,pretty_print=True, encoding="UTF-8",xml_declaration = True) #doctype='<?xml version="1.0" encoding="UTF-8"?>')
+    # print(xmlContent);
+
+    with open('pom.xml', 'wb') as fh:
+        dom.write(fh, encoding="utf-8", xml_declaration=True, pretty_print=True)
+        fh.close()
+
+    print("重新设置了: %s" % (module))
 
 
-    with open('pom.xml','w',encoding='UTF-8') as fh:
-        fh.write(dom.toxml())
-        fh.flush()
-
-
-    print("重新设置了: %s"%(module))
-
-if __name__=='__main__':
+if __name__ == '__main__':
     print("-------------------------------------------------------------------------------")
     print(os.linesep)
 
-
     setWorkPath();
 
-    tag,value,file = getArgs()
+    tag, value, file = getArgs()
     if file:
         os.chdir(file)
 
     subPoms = getSubPoms()
     for s in subPoms:
-        resetData(s,tag,value)
+        resetData(s, tag, value)
+        os.chdir("..")
 
     print("")
-    print("%s %s %s 设置完成！"%(file,tag,value))
+    print("%s %s %s 设置完成！" % (file, tag, value))
     print("-------------------------------------------------------------------------------")
-
