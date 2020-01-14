fffccc5dd0512a91d32bf3f695ca4fe97b3915a1
于新海 <iamnewsea@163.com>
2020-01-14 13:19:42 +0800

    .

--- ktmyoql/test/nbcp/testa.kt
+++ ktmyoql/test/nbcp/testa.kt
@@ -7,17 +7,8 @@ import org.junit.Test
 
 class testa : TestBase() {
 
-    @DbEntityFieldRefs(arrayOf(DbEntityFieldRef("id", "name", testa::class, "id", "name"),
-            DbEntityFieldRef("id2", "name2", testa::class, "id2", "name2")
-    ))
-    class abcd {
-
-    }
-
     @Test
     fun abc() {
-        var d = abcd();
-        var es = d::class.java.annotations
-        var df = 1;
+
     }
 }
