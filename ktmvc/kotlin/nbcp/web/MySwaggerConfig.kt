09b49c86350bb0e3154d2ee639df0150191c4da8
于新海 <iamnewsea@163.com>
2020-02-03 14:54:29 +0800

    去除 swagger映射

--- ktmvc/kotlin/nbcp/web/MySwaggerConfig.kt
+++ ktmvc/kotlin/nbcp/web/MySwaggerConfig.kt
@@ -1,17 +1,22 @@
-package nbcp.web
+//package nbcp.web
+//
+//import org.springframework.context.annotation.Configuration
+//import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport
+//
+//
+/**
+ * 很奇怪，这个配置放到单独的Jar包里，swagger-ui.html 就不能访问，
+ * 把下面的配置放到启动项目里，就起作用。
+ */
+//@Configuration
+//open class MySwaggerConfig : WebMvcConfigurationSupport() {
+//    @Override
+//    override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
+//        registry.addResourceHandler("/statics/**").addResourceLocations("classpath:/statics/");
+//        // 解决 SWAGGER 404报错
+//        registry.addResourceHandler("/swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+//        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+//    }
+//}
 
-import org.springframework.context.annotation.Configuration
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport
-
-
-@Configuration
-open class MySwaggerConfig : WebMvcConfigurationSupport() {
-    @Override
-    override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
-        registry.addResourceHandler("/statics/**").addResourceLocations("classpath:/statics/");
-        // 解决 SWAGGER 404报错
-        registry.addResourceHandler("/swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
-        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
-    }
-}
