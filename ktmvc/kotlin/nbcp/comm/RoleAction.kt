f5253346f20bed90082f825c45acc74f166340ea
于新海 <iamnewsea@163.com>
2020-03-04 10:44:15 +0800

    RoleAction

--- ktmvc/kotlin/nbcp/comm/RoleAction.kt
+++ ktmvc/kotlin/nbcp/comm/RoleAction.kt
@@ -4,12 +4,11 @@ package nbcp.comm
 /**
  * 仅允许指定角色的用户访问。
  * 注解式权限控制到 Controller！足矣。
- * 精细化到Action的权限控制，应该动态配置。
- * 原则：用户的角色只要满足注解的角色或数据库角色就允许，例：
+ * 使用该注解表示该接口的权限是固定的，是一定不可配置的。如涉及到管理员的操作。
+ * 原则：用户的角色只要满足注解的任意一种角色即可允许访问，例：
  *
  * user 的角色是 op_user
- * 如果Controller注解角色是 op_user,则所有的Action都能访问.
- * 如果Controller注解角色不是 op_user，则根据数据库动态化配置判断Action是否可访问
+ * 如果Controller注解角色是 admin,op_user,则所有的Action都能访问.
  */
 @Target(AnnotationTarget.TYPE, AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
