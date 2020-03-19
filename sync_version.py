db23a15a264b92c3959ea35436aef01fb6280804
于新海 <iamnewsea@163.com>
2020-03-19 22:14:11 +0800

    0.1.30 发版

--- sync_version.py
+++ sync_version.py
@@ -4,9 +4,8 @@
 import os
 import sys
 import shutil
-import  xml.dom.minidom
-import  getopt
-
+import xml.dom.minidom
+import getopt
 
 # 第一个参数
 newVersion = ""
@@ -14,16 +13,20 @@ if len(sys.argv) > 1:
     # 第一个参数
     newVersion = sys.argv[1]
 
+
 def err(message):
-    print("%s 发生了错误！！！" %(message))
+    print("%s 发生了错误！！！" % (message))
     sys.exit(1)
 
+
 def replaceOsSep(src):
-    return src.replace("/",os.sep).replace("\\",os.sep)
+    return src.replace("/", os.sep).replace("\\", os.sep)
+
 
 def setWorkPath():
-    base_path =  os.path.abspath( os.path.join( __file__ ,"../" ) )
-    os.chdir( base_path)
+    base_path = os.path.abspath(os.path.join(__file__, "../"))
+    os.chdir(base_path)
+
 
 def resetVersionOnly(version):
     dom = xml.dom.minidom.parse('pom.xml')
@@ -32,12 +35,12 @@ def resetVersionOnly(version):
     vv_data = root.getElementsByTagName('version')[0].childNodes[0]
     vv_data.data = version
 
-    with open('pom.xml','w',encoding='UTF-8') as fh:
+    with open('pom.xml', 'w', encoding='UTF-8') as fh:
         fh.write(dom.toxml())
         fh.flush()
 
+    print("重新设置了版本号 %s" % (version))
 
-    print("重新设置了版本号 %s" %(version) )
 
 def getVersionData():
     dom = xml.dom.minidom.parse('pom.xml')
@@ -47,57 +50,57 @@ def getVersionData():
     version = root.getElementsByTagName('version')[0].childNodes[0].data
 
     mns = filter(lambda x: x.nodeType == 1, root.getElementsByTagName('modules')[0].childNodes)
-    modules = list( map(lambda x: x.childNodes[0].data ,mns) )
+    modules = list(map(lambda x: x.childNodes[0].data, mns))
+
+    return groupId, artifactId, version, modules
 
 
-    return groupId,artifactId,version,modules
+def resetVersion(module, file, groupId, artifactId, version):
+    if module:
+        os.chdir(module)
 
-def resetVersion(module,groupId,artifactId,version):
-    os.chdir(module)
-    dom = xml.dom.minidom.parse('pom.xml')
+    dom = xml.dom.minidom.parse(file)
     root = dom.documentElement
 
     vv_data = root.getElementsByTagName('version')[0].childNodes[0]
     mns = list(filter(lambda x: x.nodeType == 1, root.getElementsByTagName('parent')[0].childNodes))
-    t_groupId= list(filter( lambda x: x.tagName == "groupId",mns ))[0].childNodes[0].data
-    t_artifactId= list(filter( lambda x: x.tagName == "artifactId",mns))[0].childNodes[0].data
-    t_version_data= list(filter( lambda x: x.tagName == "version",mns))[0].childNodes[0]
+    t_groupId = list(filter(lambda x: x.tagName == "groupId", mns))[0].childNodes[0].data
+    t_artifactId = list(filter(lambda x: x.tagName == "artifactId", mns))[0].childNodes[0].data
+    t_version_data = list(filter(lambda x: x.tagName == "version", mns))[0].childNodes[0]
 
-    if t_groupId != groupId :
+    if t_groupId != groupId:
         return
 
-    if t_artifactId != artifactId :
+    if t_artifactId != artifactId:
         return
 
     vv_data.data = version
     t_version_data.data = version
 
-    with open('pom.xml','w',encoding='UTF-8') as fh:
+    with open(file, 'w', encoding='UTF-8') as fh:
         fh.write(dom.toxml())
         fh.flush()
 
+    print("重新设置了: %s" % (module))
 
-    print("重新设置了: %s"%(module))
 
-if __name__=='__main__':
+if __name__ == '__main__':
     setWorkPath()
 
     if len(newVersion) > 0:
         resetVersionOnly(newVersion)
 
-    groupId,artifactId,version,modules = getVersionData()
+    groupId, artifactId, version, modules = getVersionData()
 
     print("-------------------------------------------------------------------------------")
     print(os.linesep)
 
+    resetVersion("", "pom_component.xml", groupId, artifactId, version)
 
-    for module in modules: 
-        resetVersion(module,groupId,artifactId,version)
+    for module in modules:
+        resetVersion(module, "pom.xml", groupId, "myoql_component", version)
         os.chdir("../")
 
-
-
     print("")
-    print("新版本 %s 设置完成！"%(version))
+    print("新版本 %s 设置完成！" % (version))
     print("-------------------------------------------------------------------------------")
-
