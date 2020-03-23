a11f86705617e75c206c850a81e4dcbd6f0b9efe
于新海 <yuxinhai@lwljuyang.com>
2020-03-23 15:43:40 +0800

    ImportantLog

--- ktext/kotlin/nbcp/base/extend/UsingScope.kt
+++ ktext/kotlin/nbcp/base/extend/UsingScope.kt
@@ -5,10 +5,12 @@ interface IDisposeable {
     fun dispose();
 }
 
-enum class LogScope(val remark:String) : IDisposeable {
+enum class LogScope(val remark: String) : IDisposeable {
+    NoLog("不记录日志"),
+    ImportantLog("重要日志"),
     ExecuteTimeNoLog("不记录执行时间"),
     AffectRowNoLog("不记录影响行数"),
-    FilterNoLog("不记录Log");   //MyAllFilter不记录任务日志
+    FilterNoLog("Filter中不记录Log");   //MyAllFilter不记录任务日志
 
     override fun dispose() {
     }
