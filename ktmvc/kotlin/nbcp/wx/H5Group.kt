6e014f1543c730c5de1d0226825fa3e801143b7f
于新海 <yuxinhai@lwljuyang.com>
2020-03-31 09:09:35 +0800

    .

--- ktmvc/kotlin/nbcp/wx/H5Group.kt
+++ ktmvc/kotlin/nbcp/wx/H5Group.kt
@@ -14,7 +14,7 @@ import java.time.LocalDateTime
 import java.util.*
 
 object H5Group {
-    @Value("\${shop.wx.appId}")
+    @Value("\${server.wx.appId}")
     lateinit var appId: String
 
     /**
