dc5ff6fd41ba89c9fed6e2b1a596f7a18cd9882d
于新海 <iamnewsea@163.com>
2020-05-04 19:11:28 +0800

    sso

--- ktmyoql/kotlin/nbcp/db/mongo/entity.kt
+++ ktmyoql/kotlin/nbcp/db/mongo/entity.kt
@@ -1,12 +1,81 @@
 package nbcp.db.mongo.entity
 
-import nbcp.db.DbEntityGroup
-import nbcp.db.IdName
+import nbcp.db.*
 import org.springframework.data.mongodb.core.mapping.Document
 import java.time.LocalDateTime
 import nbcp.db.mongo.*
 
 //--------------------------------------------------------
+/**
+ * 用户信息
+ */
+@Document
+@DbEntityGroup("MongoBase")
+@RemoveToSysDustbin
+open class SsoUser(
+        var loginName: String = "",
+        var mobile: String = "",
+        var email: String = "",
+
+        var name: String = "",
+        var logo: IdUrl = IdUrl(), //头像.
+
+        var idCard: UserIdCardData = UserIdCardData(),
+
+        var liveCity: IntCodeName = IntCodeName(),
+        var liveLocation: String = "",  //常住地
+        var corpName: String = "",
+        var job: String = "",
+        var workCity: IntCodeName = IntCodeName(),
+        var workLocation: String = ""  //工作地
+) : IMongoDocument()
+
+/**
+ * 登录信息
+ */
+@Document
+@DbEntityGroup("MongoBase")
+data class SsoLoginUser(
+        var userId: String = "",   //用户Id
+        var loginName: String = "",
+        var mobile: String = "",    //认证后更新
+        var email: String = "",     //认证后更新
+
+        var password: String = "",  // Md5Util.getBase64Md5(pwd)
+        var lastLoginAt: LocalDateTime = LocalDateTime.now(),
+
+        var authorizeCode: String = "", //授权码
+        var token: String = "",     //常用数据，也会放到主表。
+        var freshToken: String = "",
+        var authorizeCodeCreateAt: LocalDateTime = LocalDateTime.now(),
+
+        var grantApps:MutableList<IdName> = mutableListOf(),  //授权的App
+        var isLocked: Boolean = false,
+        var lockedRemark: String = ""
+) : IMongoDocument()
+
+
+/**
+ * 应用中心
+ */
+@Document
+@DbEntityGroup("MongoBase")
+data class SysApplication(
+        var key: String = "",           // 应用Id，CodeUtil.getCode()
+        var name: String = "",
+        var slogan: String = "",                    // 广告语， 每次登录的时候显示
+        var logo: IdUrl = IdUrl(),      //应用Logo
+        var siteUrl: String = "",         //展示信息，应用主站
+        var remark: String = "",
+        var hostDomainName: String = "",            // 安全域名，http 或 https 开头。
+        var secret: String = "",        //应用密钥，Api用。
+        var userUpdateHookCallbackUrl: String = "",    //用户更新后，通知App的回调。
+        var authorizeRange: MutableList<String> = mutableListOf(),  //需要授权的信息
+        var isLocked: Boolean = false,
+        var loadRemark: String = ""
+) : IMongoDocument()
+
+
 
 //系统附件表
 @Document
