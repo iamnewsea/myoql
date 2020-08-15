536cfc1dffa6a703c47676f188687ce6201a8e32
于新海 <yuxinhai@lwljuyang.com>
2020-08-15 11:08:38 +0800

    .

--- ktmvc/kotlin/nbcp/extend/ServerInfo.kt
+++ ktmvc/kotlin/nbcp/extend/ServerInfo.kt
@@ -4,22 +4,29 @@
 package nbcp.web
 
 import org.springframework.boot.web.servlet.context.ServletWebServerApplicationContext
+import org.springframework.context.ApplicationContext
 
 /**
  * Created by yuxh on 2018/7/5
  */
 
-val ServletWebServerApplicationContext.debugServerInfo: String
+val ApplicationContext.debugServerInfo: String
     get() {
-        var serverName = this.servletContext!!.serverInfo
+        var list = mutableListOf<String>()
+        if (this is ServletWebServerApplicationContext) {
+            var port = this.environment.getProperty("server.port")
+            list.add("${this.servletContext!!.serverInfo}:${port}")
+        }
         var applicationName = this.environment.getProperty("spring.application.name")
         var version = this.environment.activeProfiles.joinToString(",")
         var cloudVersion = this.environment.getProperty("spring.cloud.config.profile") ?: ""
-        var port = this.environment.getProperty("server.port")
+
 
         if (cloudVersion.isNotEmpty()) {
             version += "-cloud-" + cloudVersion
         }
 
-        return "${serverName}:${port} -- ${applicationName}:${version}"
+        list.add("${applicationName}:${version}")
+
+        return list.joinToString(" -- ");
     }
