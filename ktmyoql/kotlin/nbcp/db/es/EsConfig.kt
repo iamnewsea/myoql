73402d4763fb0ade367b38cd222c737549beb4e1
于新海 <yuxinhai@lwljuyang.com>
2020-08-20 15:08:17 +0800

    更新版本

--- ktmyoql/kotlin/nbcp/db/es/EsConfig.kt
+++ ktmyoql/kotlin/nbcp/db/es/EsConfig.kt
@@ -13,7 +13,6 @@ import org.elasticsearch.client.RestClient.FailureListener
 import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.AutoConfigureAfter
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
-import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchAutoConfiguration
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.DependsOn
