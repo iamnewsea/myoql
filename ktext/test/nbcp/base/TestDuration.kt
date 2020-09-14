bd59c8d306952c4b401d7f6076bd8b6288bf5fad
于新海 <13466396863@163.com>
2020-09-14 14:29:40 +0800

    .

--- ktext/test/nbcp/base/TestDuration.kt
+++ ktext/test/nbcp/base/TestDuration.kt
@@ -1,15 +1,24 @@
 package nbcp.base
 
 import nbcp.TestBase
+import nbcp.comm.AsLocalDateTime
 import nbcp.comm.JsonMap
+import nbcp.comm.minus
 import org.junit.Test
 import java.time.Duration
+import java.time.LocalDateTime
 
 class TestDuration : TestBase() {
     @Test
     fun Test_Duration() {
         var d = Duration.parse("PT20M");
         println(d.seconds)
+    }
+
+    @Test
+    fun Test_Duration2() {
+        var d = LocalDateTime.now().minus("2020-9-14 12:00:00".AsLocalDateTime()!!);
+        println(d.totalMinutes)
     }
 
     @Test
