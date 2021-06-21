8395eaad4336db0339ca4a4b166424ababd2dc14
于新海 <iamnewsea@163.com>
2021-06-21 19:20:41 +0800

    .

--- ktext/test/nbcp/utils/HttpUtilTest.kt
+++ ktext/test/nbcp/utils/HttpUtilTest.kt
@@ -11,10 +11,10 @@ import org.junit.Test
 class HttpUtilTest : TestBase() {
     @Test
     fun abfc() {
-        var http = HttpUtil("http://localhost:8002/sys/upload")
-        http.request.headers.set("token", "sf5ac1c8piscg0")
-        http.uploadFile("""F:\BaiduNetdiskDownload\阿里微服务架构Spring Cloud Alibaba实战-1015.mp4""")
-        println("OK")
+        var http = HttpUtil("http://localhost:8202/sys/upload")
+        http.request.headers.set("group", "lowcode")
+        var ret = http.uploadFile("""D:\能科资料\DevOps流程.vsdx""")
+        println(ret)
     }
 
     @Test
