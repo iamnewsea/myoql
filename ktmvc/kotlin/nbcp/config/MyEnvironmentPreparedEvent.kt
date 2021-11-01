e70292f697388b43d8f05f74033c0cb474eb1271
于新海 <iamnewsea@163.com>
2021-11-02 02:02:17 +0800

    .

--- ktmvc/kotlin/nbcp/config/MyEnvironmentPreparedEvent.kt
+++ ktmvc/kotlin/nbcp/config/MyEnvironmentPreparedEvent.kt
@@ -1,10 +1,16 @@
 package nbcp
 
+import nbcp.comm.JsonMap
 import org.springframework.boot.context.event.ApplicationEnvironmentPreparedEvent
+import org.springframework.boot.json.YamlJsonParser
 import org.springframework.context.ApplicationListener
+import org.springframework.core.env.MapPropertySource
+import org.springframework.core.env.StandardEnvironment
 
 class MyEnvironmentPreparedEvent : ApplicationListener<ApplicationEnvironmentPreparedEvent> {
     override fun onApplicationEvent(event: ApplicationEnvironmentPreparedEvent) {
-
+        val jsonMap = JsonMap();
+        val map = MapPropertySource("ops", jsonMap);
+        event.environment.propertySources.addBefore(StandardEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, map)
     }
 }
