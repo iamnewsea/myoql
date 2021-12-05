f27587a5d519305d19ce28ad72374cdfb8f749d9
于新海 <iamnewsea@163.com>
2021-12-05 20:56:11 +0800

    WebJsonMapper.kt

--- ktbase/test/nbcp/comm/TestKtExt_Json.kt
+++ ktbase/test/nbcp/comm/TestKtExt_Json.kt
@@ -11,6 +11,10 @@ class TestKtExt_Json : TestBase() {
         var isDeleted: Boolean? = false;
         var list = mutableListOf<IdName>();
         var ary = arrayOf<IdName>()
+
+        fun getAbc(): String {
+            return if (isDeleted == true) "dddd" else "nnnn"
+        }
     }
 
     @Test
@@ -39,7 +43,7 @@ class TestKtExt_Json : TestBase() {
         b.isDeleted = true;
 
 
-        var result   = ApiResult<Any>();
+        var result = ApiResult<Any>();
         result.data = b;
 
         var str = result.ToJson();
