2fe2811c6fbf8e993d75c6e18aec5d6f164bd87a
于新海 <yuxh@nancal.com>
2021-12-07 20:19:37 +0800

    .

--- ktmyoql/kotlin/nbcp/db/comm/BaseMetaData.kt
+++ ktmyoql/kotlin/nbcp/db/comm/BaseMetaData.kt
@@ -1,8 +1,13 @@
 package nbcp.db
 
+import nbcp.comm.HasValue
+import nbcp.db.mongo.MongoEntityCollector
 import java.io.Serializable
 
 
 abstract class BaseMetaData(var tableName: String) : Serializable{
 
+    val actualTableName by lazy {
+        db.mongo.mongoEvents.getActualTableName(tableName);
+    }
 }
