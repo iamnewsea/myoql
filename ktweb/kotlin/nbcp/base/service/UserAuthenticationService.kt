5edb35e0a7cfbd49f154e841e43a57caf2def447
于新海 <iamnewsea@163.com>
2021-12-14 01:37:22 +0800

    DefaultUserAuthenticationService

--- ktweb/kotlin/nbcp/base/service/UserAuthenticationService.kt
+++ ktweb/kotlin/nbcp/base/service/UserAuthenticationService.kt
@@ -6,6 +6,8 @@ import nbcp.db.redis.proxy.RedisStringProxy
 import org.springframework.beans.factory.InitializingBean
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.core.StringRedisTemplate
 import org.springframework.stereotype.Component
 
@@ -34,56 +36,65 @@ interface IUserAuthenticationService {
 /**
  * 表示 config.userSystem 配置的用户体系的 redis 项，格式如： {config.userSystem}token:{id}
  */
-@Component
+@Configuration
 @ConditionalOnClass(StringRedisTemplate::class)
-class DefaultUserAuthenticationService : IUserAuthenticationService {
+@ConditionalOnMissingBean(IUserAuthenticationService::class)
+class DefaultUserAuthenticationService {
+    class MyUserAuthService : IUserAuthenticationService {
 
-    /**
-     * 保存到 Redis 的 token
-     */
-    private val userSystemRedis
-        get() = RedisStringProxy(config.tokenKey, config.tokenCacheSeconds)
+        /**
+         * 保存到 Redis 的 token
+         */
+        private val userSystemRedis
+            get() = RedisStringProxy(config.tokenKey, config.tokenCacheSeconds)
 
-    /**
-     * 用户体系的图片验证码，格式如：validateCode:{id}
-     */
-    private val validateCodeRedis
-        get() = RedisStringProxy(
-                "validateCode", config.validateCodeCacheSeconds
-        )
+        /**
+         * 用户体系的图片验证码，格式如：validateCode:{id}
+         */
+        private val validateCodeRedis
+            get() = RedisStringProxy(
+                    "validateCode", config.validateCodeCacheSeconds
+            )
 
-    override fun getValidateCode(token: String): String {
-        return validateCodeRedis.get(token);
-    }
-
-    override fun setValidateCode(token: String, value: String) {
-        validateCodeRedis.set(token, value);
-    }
-
-
-    /**
-     * 获取登录token
-     */
-    override fun getLoginInfoFromToken(token: String, renewal: Boolean): LoginUserModel? {
-        if (renewal) {
-            userSystemRedis.renewalKey(token);
+        override fun getValidateCode(token: String): String {
+            return validateCodeRedis.get(token);
         }
-        return userSystemRedis.get(token).FromJson<LoginUserModel>();
-    }
 
-    /**
-     * 设置登录token,格式：{config.userSystem}token:{id}
-     */
-    override fun saveLoginUserInfo(userInfo: LoginUserModel) {
-        if (userInfo.id.HasValue && userInfo.token.HasValue) {
-            userSystemRedis.set(userInfo.token, userInfo.ToJson())
+        override fun setValidateCode(token: String, value: String) {
+            validateCodeRedis.set(token, value);
+        }
+
+
+        /**
+         * 获取登录token
+         */
+        override fun getLoginInfoFromToken(token: String, renewal: Boolean): LoginUserModel? {
+            if (renewal) {
+                userSystemRedis.renewalKey(token);
+            }
+            return userSystemRedis.get(token).FromJson<LoginUserModel>();
+        }
+
+        /**
+         * 设置登录token,格式：{config.userSystem}token:{id}
+         */
+        override fun saveLoginUserInfo(userInfo: LoginUserModel) {
+            if (userInfo.id.HasValue && userInfo.token.HasValue) {
+                userSystemRedis.set(userInfo.token, userInfo.ToJson())
+            }
+        }
+
+        /**
+         * 删除tokens
+         */
+        override fun deleteToken(vararg tokens: String) {
+            userSystemRedis.deleteKeys(*tokens)
         }
     }
 
-    /**
-     * 删除tokens
-     */
-    override fun deleteToken(vararg tokens: String) {
-        userSystemRedis.deleteKeys(*tokens)
+    @Bean
+    fun userAuthService(): MyUserAuthService {
+        return MyUserAuthService();
     }
 }
+
