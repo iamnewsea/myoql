bb5d60bddf2efa2245b2c2e6b4a4287b33e50606
于新海 <yuxh@nancal.com>
2021-12-14 18:25:59 +0800

    RedisJsonProxy

---
+++ ktmyoql/kotlin/nbcp/db/redis/proxy/RedisJsonProxy.kt
@@ -0,0 +1,43 @@
+package nbcp.db.redis.proxy
+
+import nbcp.comm.*
+import nbcp.utils.*
+import nbcp.db.redis.BaseRedisProxy
+import nbcp.db.redis.RedisRenewalTypeEnum
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.core.StringRedisTemplate
+import org.springframework.data.redis.core.ValueOperations
+import java.io.Serializable
+import java.time.Duration
+
+/**
+ * Created by udi on 17-7-14.
+ */
+class RedisJsonProxy<T:Serializable> @JvmOverloads constructor(
+        group: String,
+        val clazz:Class<T>,
+        defaultCacheSeconds: Int = 0) :
+        BaseRedisProxy(group, defaultCacheSeconds) {
+
+
+    fun get(key: String = ""): T? {
+        var cacheKey = getFullKey(key)
+        var value = stringCommand.opsForValue().get(cacheKey)
+        if (value == null) return null;
+        return value.FromJson(clazz)
+    }
+
+//    fun setKey(value: String, cacheSecond: Int = defaultCacheSeconds) = setKey("", value, cacheSecond);
+
+    fun set(key: String, value: T, cacheSecond: Int = defaultCacheSeconds) {
+        var cacheKey = getFullKey(key)
+
+        if (cacheSecond <= 0) {
+            stringCommand.opsForValue().set(cacheKey, value.ToJson())
+        } else {
+            stringCommand.opsForValue().set(cacheKey, value.ToJson(), Duration.ofSeconds(cacheSecond.AsLong()))
+        }
+    }
+}
+
+
