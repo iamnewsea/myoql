f25e825bcc318e421038e5f6321ebb07c60f07e0
于新海 <yuxh@nancal.com>
2021-12-20 10:03:25 +0800

    多数据源重构

--- ktmyoql/kotlin/nbcp/bean/MyOqlMongoBeanConfig.kt
+++ ktmyoql/kotlin/nbcp/bean/MyOqlMongoBeanConfig.kt
@@ -1,29 +1,17 @@
 package nbcp.bean
 
-import com.mongodb.ConnectionString
 import com.mongodb.MongoClientSettings
-import com.mongodb.client.MongoClient
 import nbcp.comm.HasValue
 import nbcp.comm.getStringValue
-import nbcp.db.db
 import nbcp.db.mongo.Date2LocalDateTimeConverter
-import nbcp.db.mongo.service.UploadFileMongoService
 import nbcp.utils.JsUtil
-import nbcp.utils.SpringUtil
 import org.bson.UuidRepresentation
-import org.bson.codecs.UuidCodecProvider
-import org.bson.codecs.configuration.CodecRegistries
-import org.bson.codecs.configuration.CodecRegistry
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.config.BeanPostProcessor
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass
 import org.springframework.boot.autoconfigure.mongo.MongoProperties
-import org.springframework.boot.context.event.ApplicationReadyEvent
-import org.springframework.context.annotation.Import
-import org.springframework.context.event.EventListener
 import org.springframework.core.convert.support.GenericConversionService
 import org.springframework.data.mongodb.MongoDatabaseFactory
-import org.springframework.data.mongodb.MongoTransactionManager
 import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper
 import org.springframework.data.mongodb.core.convert.MappingMongoConverter
@@ -43,7 +31,6 @@ class MyOqlMongoBeanConfig : BeanPostProcessor {
             var converter = bean.converter as MappingMongoConverter;
             converter.typeMapper = DefaultMongoTypeMapper(null)
             (converter.conversionService as GenericConversionService).addConverter(Date2LocalDateTimeConverter())
-        } else if (bean is MongoClient) {
         } else if (bean is MongoClientSettings) {
         } else if (bean is MongoProperties) {
             //修改默认连接池参数
