11ba09f454669d8ef353bc4230eddd201f7d8622
于新海 <iamnewsea@163.com>
2022-01-19 00:57:26 +0800

    .

--- ktagent/kotlin/nbcp/AgentMain.kt
+++ ktagent/kotlin/nbcp/AgentMain.kt
@@ -1,20 +1,16 @@
 package nbcp
 
+import java.lang.instrument.ClassDefinition
 import java.lang.instrument.Instrumentation
 
 object AgentMain {
 
     @JvmStatic
     fun premain(agentArgs: String?, inst: Instrumentation) {
-
         work(inst);
     }
 
     private fun work(inst: Instrumentation) {
         inst.addTransformer(MyTransform(), true)
-
-        inst.allLoadedClasses.map {
-            return@map it.name
-        }
     }
 }
