99507a20e3f830296850b8330a1ae0ca652c2970
于新海 <yuxh@nancal.com>
2022-01-19 16:26:51 +0800

    .

--- tools/ktembed/kotlin/nbcp/MyOqlDependencyInitConfig.kt
+++ tools/ktembed/kotlin/nbcp/MyOqlDependencyInitConfig.kt
@@ -1,51 +1,111 @@
 package nbcp
 
+import nbcp.comm.AsString
+import nbcp.comm.HasValue
+import nbcp.comm.IsSimpleType
 import nbcp.comm.ifTrue
 import nbcp.utils.ClassUtil
+import nbcp.utils.MyUtil
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.config.BeanPostProcessor
 import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.context.annotation.*
 import org.springframework.context.event.EventListener
-import org.springframework.data.mongodb.core.MongoTemplate
+import java.lang.reflect.Proxy
 
 @Configuration
-class MyOqlDependencyInitConfig {
+class MyOqlDependencyInitConfig : BeanPostProcessor {
     companion object {
+        private var inited = false;
+        private var dependency = DependencyData();
         private val logger = LoggerFactory.getLogger(this::class.java.declaringClass)
     }
 
-    @EventListener
-    fun prepared(ev: ApplicationReadyEvent) {
-        var list = mutableListOf<BaseComponentEnum>()
+    class DependencyData(
+        var components: MutableList<BaseComponentEnum> = mutableListOf(),
+        var services: MutableList<String> = mutableListOf()
+    )
+
+    override fun postProcessBeforeInitialization(bean: Any, beanName: String): Any? {
+        if (inited == false) {
+            inited = true;
+
+            init_app();
+        }
+        return super.postProcessBeforeInitialization(bean, beanName)
+    }
+
+    override fun postProcessAfterInitialization(bean: Any, beanName: String): Any? {
+        //收集Feign
+        if (dependency.components.contains(BaseComponentEnum.Feign)) {
+            var beanType = bean::class.java;
+            var feign =
+                beanType.annotations.firstOrNull { it.annotationClass.qualifiedName == "org.springframework.cloud.openfeign.FeignClient" }
+
+            if (feign != null) {
+                var url = MyUtil.getValueByWbsPath(feign, "url").AsString();
+                if (url.HasValue) {
+                    dependency.services.add(url)
+                } else {
+                    var value = MyUtil.getValueByWbsPath(feign, "value").AsString();
+                    if (value.HasValue) {
+                        dependency.services.add(value)
+                    }
+                }
+            }
+        }
+
+        return super.postProcessAfterInitialization(bean, beanName)
+    }
+
+    private fun init_app() {
+        dependency = getDependencys();
+    }
+
+
+    /**
+     * 保存
+     */
+    private fun push(dependency: DependencyData) {
+
+    }
+
+    /**
+     * 收集依赖
+     */
+    private fun getDependencys(): DependencyData {
+        var dependency = DependencyData();
 
         ClassUtil.exists("org.springframework.data.redis.core.RedisTemplate").ifTrue {
-            list.add(BaseComponentEnum.Redis)
+            dependency.components.add(BaseComponentEnum.Redis)
         }
 
         ClassUtil.exists("org.springframework.data.mongodb.core.MongoTemplate").ifTrue {
-            list.add(BaseComponentEnum.Mongo)
+            dependency.components.add(BaseComponentEnum.Mongo)
         }
 
         ClassUtil.exists("org.springframework.jdbc.core.JdbcTemplate").ifTrue {
-            list.add(BaseComponentEnum.Jdbc)
+            dependency.components.add(BaseComponentEnum.Jdbc)
 
 
         }
         ClassUtil.exists("org.springframework.amqp.rabbit.core.RabbitTemplate").ifTrue {
-            list.add(BaseComponentEnum.Rabbitmq)
+            dependency.components.add(BaseComponentEnum.Rabbitmq)
         }
 
         ClassUtil.exists("org.elasticsearch.client.RestClientBuilder").ifTrue {
-            list.add(BaseComponentEnum.Es)
+            dependency.components.add(BaseComponentEnum.Es)
         }
 
         ClassUtil.exists("org.springframework.cloud.openfeign.FeignClient").ifTrue {
-            list.add(BaseComponentEnum.Feign)
+            dependency.components.add(BaseComponentEnum.Feign)
         }
 
         ClassUtil.exists("io.minio.MinioClient").ifTrue {
-            list.add(BaseComponentEnum.Minio)
+            dependency.components.add(BaseComponentEnum.Minio)
         }
+
+
+        return dependency;
     }
 }
