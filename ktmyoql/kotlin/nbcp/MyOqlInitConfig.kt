9799628f17b74139cf1b91bca94e7107f8c9404a
于新海 <yuxh@nancal.com>
2022-03-02 17:32:23 +0800

    .

--- ktmyoql/kotlin/nbcp/MyOqlInitConfig.kt
+++ ktmyoql/kotlin/nbcp/MyOqlInitConfig.kt
@@ -2,40 +2,13 @@ package nbcp
 
 import nbcp.bean.*
 import nbcp.comm.Important
-import nbcp.component.BaseJsonMapper
-import nbcp.component.DbJsonMapper
-import nbcp.db.mongo.event.*
-import nbcp.db.mybatis.MyBatisRedisCachePointcutAdvisor
-import nbcp.db.mybatis.MybatisDbConfig
-import nbcp.db.mysql.MySqlDataSourceConfig
-import nbcp.db.MyOqlBaseActionLogDefine
-import nbcp.db.MyOqlMultipleDataSourceDefine
-import nbcp.db.cache.RedisCacheAopService
 import nbcp.db.db
-import nbcp.db.es.*
-import nbcp.db.mongo.*
-import nbcp.db.sql.*
-import nbcp.db.sql.event.*
-import nbcp.model.IUploadFileDbService
 import nbcp.utils.SpringUtil
-import org.bson.Document
-import org.bson.types.ObjectId
 import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.BeanFactory
-import org.springframework.beans.factory.BeanFactoryAware
-import org.springframework.beans.factory.config.BeanDefinitionHolder
 import org.springframework.beans.factory.config.BeanPostProcessor
-import org.springframework.beans.factory.support.BeanDefinitionRegistry
-import org.springframework.boot.context.event.ApplicationPreparedEvent
 import org.springframework.boot.context.event.ApplicationStartedEvent
-import org.springframework.context.ApplicationEvent
-import org.springframework.context.ResourceLoaderAware
 import org.springframework.context.annotation.*
 import org.springframework.context.event.EventListener
-import org.springframework.core.io.ResourceLoader
-import org.springframework.core.type.AnnotationMetadata
-import org.springframework.core.type.filter.AssignableTypeFilter
-import org.springframework.stereotype.Component
 
 @Import(FlywayBeanProcessor::class)
 @Configuration
