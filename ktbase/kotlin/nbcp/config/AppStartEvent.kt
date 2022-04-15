385ea5653961b0da15d8b8d591b6874b7a18e231
于新海 <yuxh@nancal.com>
2022-04-15 17:36:03 +0800

    .

--- ktbase/kotlin/nbcp/config/AppStartEvent.kt
+++ ktbase/kotlin/nbcp/config/AppStartEvent.kt
@@ -4,10 +4,11 @@ import org.springframework.boot.context.event.ApplicationStartingEvent
 import org.springframework.context.ApplicationListener
 
 /**
- * 配置项
+ * 程序的最高事件。
  */
 class AppStartEvent : ApplicationListener<ApplicationStartingEvent> {
     override fun onApplicationEvent(event: ApplicationStartingEvent) {
-        
+        //该方法在环境初始化之前， 可以使额外的 bootstrap- 项生效！
+//        event.springApplication.setAdditionalProfiles("k8s")
     }
 }
