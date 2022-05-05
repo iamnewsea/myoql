ce4337596fbf567aafdec16861f58a5328500b82
于新海 <yuxh@nancal.com>
2022-05-05 16:36:19 +0800

    .

--- ktbase/kotlin/nbcp/KotlinExtendInitConfig.kt
+++ ktbase/kotlin/nbcp/KotlinExtendInitConfig.kt
@@ -11,6 +11,9 @@ import org.springframework.stereotype.Service
 
 //@Configuration
 
+/**
+ * 不能在这里 Import 任何Bean, 因为这个Bean的时机太早了, 会导致 Import 的目标对象不生效.
+ */
 class KotlinExtendInitConfig : BaseImportBeanDefinitionRegistrar(
     "nbcp",
     listOf(
