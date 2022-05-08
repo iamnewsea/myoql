23f1ab2a926676b1aaee0accd7da3dfeacc1b5ea
于新海 <yuxh@nancal.com>
2022-05-09 00:05:20 +0800

    .

--- ktbase/kotlin/nbcp/db/MemoryCacheDb.kt
+++ ktbase/kotlin/nbcp/db/MemoryCacheDb.kt
@@ -2,6 +2,7 @@ package nbcp.db
 
 import nbcp.comm.JsonMap
 import nbcp.comm.plusSeconds
+import org.slf4j.LoggerFactory
 import java.time.LocalDateTime
 import java.util.function.Supplier
 
@@ -9,8 +10,10 @@ import java.util.function.Supplier
  * 常驻内存型缓存
  */
 object MemoryCacheDb {
-    var warnCapacity: Int = 100
-    var errorCapacity: Int = 1000
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
+    var warnEachCapacity: Int = 100
+    var errorMaxCapacity: Int = 1000
 
     private val map = JsonMap()
 
@@ -20,6 +23,16 @@ object MemoryCacheDb {
     }
 
     fun addItem(key: String, cacheSeconds: Int = 180, callback: Supplier<Any>) {
+        if (map.size > 0) {
+            if (map.size >= errorMaxCapacity) {
+                throw java.lang.RuntimeException("缓存数据已达到最大条数 ${map.size} 条!")
+            }
+
+            if (map.size % warnEachCapacity == 0) {
+                logger.warn("缓存数据达到 ${map.size} 条!")
+            }
+        }
+
         map.put(key, CacheItem(callback, cacheSeconds))
     }
 
