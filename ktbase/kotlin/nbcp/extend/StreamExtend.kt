f03bd9f4b88c08599363536993596bae12656580
于新海 <yuxh@nancal.com>
2022-05-16 13:29:34 +0800

    .

--- ktbase/kotlin/nbcp/extend/StreamExtend.kt
+++ ktbase/kotlin/nbcp/extend/StreamExtend.kt
@@ -4,6 +4,7 @@
 package nbcp.comm
 
 import java.io.*
+import java.nio.ByteBuffer
 
 
 /**
@@ -13,20 +14,29 @@ fun InputStream.readContentString(): String {
     return String(this.readBytes(), const.utf8)
 }
 
+/**
+ * ByteBuffer 转 ByteArray
+ */
+fun ByteBuffer.toByteArray(): ByteArray {
+    val copy = ByteArray(this.remaining())
+    this.get(copy)
+    return copy
+}
+
 
 /**
  * 把流转化为可重复读的流。
  * @return 可能返回 BufferedInputStream! (效果比ByteArrayInputStream好)
  */
-fun InputStream.prepareToMarkSupportedInputStream() :InputStream{
-    if( this.markSupported()) return this;
+fun InputStream.prepareToMarkSupportedInputStream(): InputStream {
+    if (this.markSupported()) return this;
     return BufferedInputStream(this);
 }
 
 /**
  * 把流全部读入内存，实现可重复读，内存压力会大
  */
-fun InputStream.readToMemoryStream() :ByteArrayInputStream{
+fun InputStream.readToMemoryStream(): ByteArrayInputStream {
     this.use {
         return ByteArrayInputStream(this.readBytes());
     }
