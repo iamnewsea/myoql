c471350cb9c21c3833d258b70f26275bf91c7b86
于新海 <yuxh@nancal.com>
2022-08-17 18:07:26 +0800

    .

--- ktbase/kotlin/nbcp/comm/MyJsonUtil.java
+++ ktbase/kotlin/nbcp/comm/MyJsonUtil.java
@@ -8,16 +8,16 @@ import nbcp.scope.JsonStyleEnumScope;
  * 仅为了Java规范
  */
 public class MyJsonUtil {
-    public <T> String toJson(T value) {
+    public static <T> String toJson(T value) {
         return MyJson.ToJson(value, null, new JsonStyleEnumScope[0]);
     }
 
-    public <T> String toJson(T value, JsonSceneEnumScope style, JsonStyleEnumScope... jsonScopes) {
+    public static <T> String toJson(T value, JsonSceneEnumScope style, JsonStyleEnumScope... jsonScopes) {
         return MyJson.ToJson(value, style, jsonScopes);
     }
 
 
-    public <T> T fromJson(String value, Class<T> clazz) {
+    public static <T> T fromJson(String value, Class<T> clazz) {
         return MyJson.FromJson(value, clazz);
     }
 }
