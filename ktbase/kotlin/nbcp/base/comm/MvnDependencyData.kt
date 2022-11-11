2336df3c92041fef6209ddbbeb0c86b4283b4c4f
于新海 <yuxh@nancal.com>
2022-11-11 11:43:47 +0800

    .

--- ktbase/kotlin/nbcp/base/comm/MvnDependencyData.kt
+++ ktbase/kotlin/nbcp/base/comm/MvnDependencyData.kt
@@ -4,8 +4,8 @@ package nbcp.base.comm
  * Mvn依赖定义
  */
 open class MvnDependencyBasicData(
-    var groupId: String,
-    var artifactId: String,
-    var version: String,
-    var scope: String
+    var groupId: String = "",
+    var artifactId: String = "",
+    var version: String = "",
+    var scope: String = ""
 )
