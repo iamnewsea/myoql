82dfdfd85ab478555233afac1047900603679b07
于新海 <yuxh@nancal.com>
2022-12-08 01:58:16 +0800

    .

--- ktbase/kotlin/nbcp/base/utils/MyUtil.kt
+++ ktbase/kotlin/nbcp/base/utils/MyUtil.kt
@@ -405,7 +405,7 @@ object MyUtil {
     /**
      * 深度自由的克隆。可能有性能问题！
      */
-    @Deprecated
+    @Deprecated("未完成！")
     fun deepFreeClone(source: Any, target: Any, maxDepth: Int = 32, action: ((Any, Any) -> Boolean)? = null) {
         //TODO:
     }
