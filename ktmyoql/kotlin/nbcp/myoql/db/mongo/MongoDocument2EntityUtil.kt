596d47a17a7c8da5dd8a74d50dc219f1e53d67b6
于新海 <iamnewsea@163.com>
2023-03-29 01:59:36 +0800

    .

--- ktmyoql/kotlin/nbcp/myoql/db/mongo/MongoDocument2EntityUtil.kt
+++ ktmyoql/kotlin/nbcp/myoql/db/mongo/MongoDocument2EntityUtil.kt
@@ -99,63 +99,63 @@ object MongoDocument2EntityUtil {
      */
     @JvmStatic
     fun procDocumentJson(value: Document) {
-        fun testDocumentString(item: Any?): Boolean {
-            if (item == null) return false;
-            var type = item::class.java;
-            if (type.IsStringType == false) return false;
-            var v_string_value = item.toString()
-            return v_string_value.contains("{{") && v_string_value.endsWith("}}")
-        }
-
-        fun procDocumentString(v_string_value: String): Any {
-            //Document{{answerRole=Patriarch}}
-            //目前只发现一个键值对形式的。
-            val startIndex = v_string_value.indexOf("{{");
-            if (startIndex < 0) {
-                return v_string_value;
-            }
-
-            logger.Important("发现mongo Document怪异值: " + v_string_value);
-            val json = StringMap();
-            v_string_value.Slice(startIndex + 2, -2).split(",").forEach { item ->
-                val sect = item.split("=");
-                json.put(sect[0], sect[1]);
-            }
-            return json;
-        }
-
-        procResultData_id2Id(value);
-
-        RecursionUtil.recursionAny(value, { json ->
-            json.keys.toTypedArray().forEachIndexed { _, key ->
-                if (key == null) {
-                    return@forEachIndexed
-                }
-                var documentStringValue = json.get(key);
-                if (!testDocumentString(documentStringValue)) {
-                    return@forEachIndexed
-                }
-
-                logger.Important("发现怪异mongo Document: " + value.ToJson())
-                logger.Important("发现怪异mongo Document,当前: " + json.ToJson())
-                (json as MutableMap<Any, Any>).set(key, procDocumentString(documentStringValue.toString()));
-
-                return@forEachIndexed
-            }
-            return@recursionAny true
-        }, { list ->
-            var arrayList = list as MutableList<Any?>
-            arrayList.forEachIndexed { index, it ->
-                if (it == null || !testDocumentString(it)) {
-                    return@forEachIndexed
-                }
-
-                logger.Important("发现怪异mongo Document: " + value.ToJson())
-                logger.Important("发现怪异mongo Array[${index}]: " + it.ToJson())
-                arrayList[index] = procDocumentString(it.toString());
-            }
-
-            return@recursionAny true
-        })
+//        fun testDocumentString(item: Any?): Boolean {
+//            if (item == null) return false;
+//            var type = item::class.java;
+//            if (type.IsStringType == false) return false;
+//            var v_string_value = item.toString()
+//            return v_string_value.contains("{{") && v_string_value.endsWith("}}")
+//        }
+//
+//        fun procDocumentString(v_string_value: String): Any {
+//            //Document{{answerRole=Patriarch}}
+//            //目前只发现一个键值对形式的。
+//            val startIndex = v_string_value.indexOf("{{");
+//            if (startIndex < 0) {
+//                return v_string_value;
+//            }
+//
+//            logger.Important("发现mongo Document怪异值: " + v_string_value);
+//            val json = StringMap();
+//            v_string_value.Slice(startIndex + 2, -2).split(",").forEach { item ->
+//                val sect = item.split("=");
+//                json.put(sect[0], sect[1]);
+//            }
+//            return json;
+//        }
+//
+//        procResultData_id2Id(value);
+//
+//        RecursionUtil.recursionAny(value, { json ->
+//            json.keys.toTypedArray().forEachIndexed { _, key ->
+//                if (key == null) {
+//                    return@forEachIndexed
+//                }
+//                var documentStringValue = json.get(key);
+//                if (!testDocumentString(documentStringValue)) {
+//                    return@forEachIndexed
+//                }
+//
+//                logger.Important("发现怪异mongo Document: " + value.ToJson())
+//                logger.Important("发现怪异mongo Document,当前: " + json.ToJson())
+//                (json as MutableMap<Any, Any>).set(key, procDocumentString(documentStringValue.toString()));
+//
+//                return@forEachIndexed
+//            }
+//            return@recursionAny true
+//        }, { list ->
+//            var arrayList = list as MutableList<Any?>
+//            arrayList.forEachIndexed { index, it ->
+//                if (it == null || !testDocumentString(it)) {
+//                    return@forEachIndexed
+//                }
+//
+//                logger.Important("发现怪异mongo Document: " + value.ToJson())
+//                logger.Important("发现怪异mongo Array[${index}]: " + it.ToJson())
+//                arrayList[index] = procDocumentString(it.toString());
+//            }
+//
+//            return@recursionAny true
+//        })
     }
 }
