8f757eab1c2ed25cef6f84638291efae2aa90066
于新海 <iamnewsea@163.com>
2023-03-28 14:16:38 +0800

    .

--- help/data/export-mongo.sh
+++ help/data/export-mongo.sh
@@ -6,14 +6,14 @@ password=1234.5678
 time=$(date "+%Y%m%d-%H%M")
 filter="."
 
-mkdir -p /data/mongo/$time
+mkdir -p /data/mongo-bak/$time
 mongo --host $host --quiet $database -u $user -p $password --authenticationDatabase "admin" --eval "db.getCollectionNames()" \
-  | awk '{print substr($0,3,length()-4)}' \
+  | awk '{gsub(/[",]/,"");print substr($0,2)}' \
   | grep $filter \
   > mongo-tables.txt
 
 for table in `cat mongo-tables.txt` ; do
-  mongoexport --host $host -d $database -u $user -p $password --collection $table  -o /data/mongo/$time/$table.json && echo "[$table done!]"
+  mongoexport --host $host -d $database -u $user -p $password  --authenticationDatabase "admin" --collection $table  -o /data/mongo-bak/$time/$table.json && echo "[$table done!]"
 done
 
 echo "mongo数据导出完成！"
