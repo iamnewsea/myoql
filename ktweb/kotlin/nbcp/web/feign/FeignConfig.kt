9153903b896ca48a526f66f0ad75fc5792c4ca94
于新海 <iamnewsea@163.com>
2023-07-25 21:25:24 +0800

    feign + log

--- ktweb/kotlin/nbcp/web/feign/FeignConfig.kt
+++ ktweb/kotlin/nbcp/web/feign/FeignConfig.kt
@@ -4,12 +4,11 @@ import feign.Client
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
-@Configuration
+
 class FeignConfig {
 
     @Bean
-    fun feignClient(
-    ): TraceFeignClientDecorator? {
+    fun traceFeignClient(): TraceFeignClientDecorator {
         return TraceFeignClientDecorator(Client.Default(null, null))
     }
 }
