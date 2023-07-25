c8e212f86281c0bdf518a7c88387613dcf30165e
于新海 <iamnewsea@163.com>
2023-07-25 08:48:08 +0800

    + DockerFeignClient

--- ktweb/kotlin/nbcp/web/fiegn/DockerFeignClient.kt
+++ ktweb/kotlin/nbcp/web/fiegn/DockerFeignClient.kt
@@ -2,10 +2,67 @@ package nbcp.web.fiegn
 
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
 
-
-@FeignClient(name = "docker-feign", url = "\${app.feign.docker.url}")
+/**
+ * Docker Api:
+ * https://docs.docker.com/engine/api/v1.42/#section/Authentication
+ */
+@FeignClient(name = "docker-feign", url = "\${app.feign.docker-api-url}")
 interface DockerFeignClient {
+    class DockerContainer {
+        class DockerNetwork {
+            var NetworkID = "";
+            var Gateway = "";
+            var IPAddress = "";
+        }
+
+        class DockerNetworkSettings {
+            var Networks = mapOf<String, DockerNetwork>()
+        }
+
+        class DockerMount {
+            var Type = "";
+            var Source = "";
+            var Destination = "";
+            var Propagation = "";
+            var RW: Boolean = false;
+        }
+
+        var Id = "";
+        var Image = "";
+        var Created: Long = 0;
+        var Names = listOf<String>()
+        var State = "";
+        var Status = "";
+        var NetworkSettings = DockerNetworkSettings();
+
+        var Mounts = listOf<DockerMount>();
+    }
+
     @GetMapping("/containers/json")
-    fun contains(): String
+    fun contains(): List<DockerContainer>
+
+    @PostMapping("/containers/{id}/kill")
+    fun kill(@PathVariable id: String)
+
+    @PostMapping("/containers/{id}/start")
+    fun start(@PathVariable id: String)
+
+
+    class ContainerJson {
+        var Created = "";
+        var State = DockerState();
+
+        class DockerState {
+            var Status = "";
+            var Running: Boolean? = null;
+            var Error: String = "";
+            var StartedAt = "";
+        }
+    }
+
+    @GetMapping("/containers/{id}/json")
+    fun inspect(@PathVariable id: String): ContainerJson
 }
