48cfa21026e86181f73a909d83160e54aac9577b
于新海 <iamnewsea@163.com>
2023-10-05 09:33:59 +0800

    升级版本

--- ktbase/kotlin/nbcp/base/utils/TokenUtil.kt
+++ ktbase/kotlin/nbcp/base/utils/TokenUtil.kt
@@ -5,24 +5,17 @@ import java.time.LocalDateTime
 object TokenUtil {
 
     /**
-     * token前缀,SnowFlakeToken, 加上了一个 encodeURIComponent 不会转义的字符： - _ . ! ~ * ' ( )
-     */
-    private val tokenPrefix = "st!";
-
-    /**
      * 根据用户体系生成新的 token。
      */
     @JvmStatic
     fun generateToken(): String {
-        return tokenPrefix + CodeUtil.getCode();
+        return  CodeUtil.getCode();
     }
 
     @JvmStatic
     fun getTokenCreateTime(token: String): LocalDateTime? {
-        if (token.startsWith(tokenPrefix) == false) return null;
-
         try {
-            return CodeUtil.getDateTimeFromCode(token.split("!")[1]);
+            return CodeUtil.getDateTimeFromCode(token);
         } catch (e: Exception) {
             return null;
         }
